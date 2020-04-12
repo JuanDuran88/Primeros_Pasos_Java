15bee4aeff073fe56e5c86a99e9b22a4c9b61643
Juan Manuel Duran <juanmanuel03@gmail.com>
2020-04-12 16:19:14 -0500

    Java Course

--- PrimerosPasos/src/poo/Uso_Empleado.java
+++ PrimerosPasos/src/poo/Uso_Empleado.java
@@ -26,12 +26,20 @@ public class Uso_Empleado {
 		System.out.println("Nombre: " + empleado3.dameNombre() + " - Sueldo: " + empleado3.dameSueldo()
 				+ " - Fecha de Alta: " + empleado3.dameFechaContrato());*/
 		
-		Empleado[] misEmpleados=new Empleado[4];
+		Jefatura jefe_RRHH=new Jefatura("Juan", 55000, 2006,9,25);
+		
+		jefe_RRHH.estableceIncentivo(2570);
+		
+		Empleado[] misEmpleados=new Empleado[6];
 		
 		misEmpleados[0]=new Empleado("Paco Gómez", 85000, 1990, 12, 17);
 		misEmpleados[1]=new Empleado("Ana López", 95000, 1995, 06, 02);
 		misEmpleados[2]=new Empleado("María Martín", 105000, 2002, 03, 15);
 		misEmpleados[3]=new Empleado("Antonio Fernandez");
+		misEmpleados[4]=jefe_RRHH; //Polimorfismo en acción. Principio de sustitución. Almacenar en esta posición un objeto de tipo empleado, sin embargo almacenamos un objeto de tipo jefatura.
+		                           //Podemos utilizar un objeto de la subclase cuando el programa espere un objeto de la superclase. Regla de escun
+		misEmpleados[5]=new Jefatura("Maria",95000, 1999,5,26);
+		
 		
 		/*for(int i=0;i<3;i++) {
 			
